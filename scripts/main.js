e40bf2b3422b08356d7333ec3760ec30e0095b8f
jhoneber31 <jhon.huarcaya@alticsa.com>
2024-10-29 21:56:02 -0500

    [UPDATE] header responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("main.js loaded");
   fetch("./header/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.querySelector("header").innerHTML = data;
+
+      const menuBurger = document.getElementById("burger-icon");
+
+      if(menuBurger) {
+        menuBurger.addEventListener("click", function () {
+          document.getElementById("header__box").classList.toggle("header__box-active");
+        });
+      }
     });
 });
