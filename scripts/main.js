1fd97b272fdd3c7786472c33a846febfe6c11d19
edwindeza <ejdeza@gmail.com>
2024-10-29 21:00:09 -0500

    [UPDATE] structure files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("main.js loaded");
+  fetch("./header/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("header").innerHTML = data;
+    });
+});
--- src/modules/shared/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  console.log('main.js loaded');
-  fetch('../header/header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.querySelector('header').innerHTML = data;
-    });
-});
