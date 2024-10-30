9fa84c3f2749a9ea7fbb42b2227bbb930b6d3eed
jhoneber31 <jhon.huarcaya@alticsa.com>
2024-10-29 22:10:41 -0500

    [UPDATE] header active

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector("header").innerHTML = data;
 
       const menuBurger = document.getElementById("burger-icon");
+      const links = document.querySelectorAll(".header__link");
 
       if(menuBurger) {
         menuBurger.addEventListener("click", function () {
           document.getElementById("header__box").classList.toggle("header__box-active");
+        });
+      }
+
+      if(links) {
+        const currentUrl = window.location.pathname;
+        console.log({ currentUrl });
+        links.forEach((link) => {
+          const aHref = link.querySelector("a").getAttribute("href");
+          const linkActive = new URL(aHref, window.location.origin).pathname
+
+          console.log({ linkActive });
+
+          if (currentUrl === linkActive) {
+            link.classList.add("header__link-active");
+          }
         });
       }
     });
